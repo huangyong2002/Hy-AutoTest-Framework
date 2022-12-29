a4f3cb1e08b7636156301de1e49c883b667ca89d
henty <2175241598@qq.com>
2022-12-29 19:59:01 +0800

    利用PyYAML根据获取的项目绝对路径实现对YAML文件的读取

--- common/tools.py
+++ common/tools.py
@@ -1,11 +1,39 @@
 # @Time:2022/12/27 12:31
 # @Author:Henry
 import datetime
+import os
 
 
 def get_now_time():
     return datetime.datetime.now()
 
 
+def get_project_path():
+    """
+    获取项目绝对路径
+    :return:
+    """
+    project_name = "CampusTrading-ui-autotest"
+    file_path = os.path.dirname(__file__)
+    # print(file_path)
+    # print(file_path.find(project_name))
+    # print(file_path[:file_path.find(project_name)+len(project_name)])
+
+    return file_path[:file_path.find(project_name) + len(project_name)]
+
+
+def sep(path, add_sep_before=False, add_sep_after=False):
+    all_path = os.sep.join(path)
+
+    if add_sep_before:
+        all_path = os.sep+all_path
+    if add_sep_after:
+        all_path = all_path+os.sep
+    print(all_path)
+    return all_path
+
+
 if __name__ == '__main__':
-    print(get_now_time())
+    # print(get_now_time())
+    # print(get_project_path())
+    sep(["config", "environment.yaml"],add_sep_before=True,add_sep_after=True)
---
+++ common/yaml_config.py
@@ -0,0 +1,33 @@
+# @Time:2022/12/29 16:59
+# @Author:Henry
+# file = open(r"D:\code\trading_system_autotest\config\environment.yaml", encoding="utf-8")
+# try:
+#     a = file.read()
+#     print(a)
+# except Exception as e:
+#     print(e)
+# finally:
+#     file.close()
+# with open(r"D:\code\trading_system_autotest\config\environment.yaml", "r",
+#           encoding="utf-8") as file:
+#     # a = file.read()
+#     for i in file.readlines():
+#         print("======")
+#         print(i)
+import yaml
+from common.tools import get_project_path,sep
+
+
+class GetConf:
+    def __init__(self):
+        with open(get_project_path()+sep(["config","environment.yaml"],add_sep_before=True), "r",
+                  encoding="utf-8") as env_file:
+            self.env = yaml.load(env_file,Loader=yaml.FullLoader)
+            #print(self.env)
+
+    def get_username_password(self):
+        return self.env["username"],self.env["password"]
+
+
+if __name__ == '__main__':
+    print(GetConf().get_username_password())
