ca7b4d2ac202a8ed949b25dc245697f5d3432aba
henty <2175241598@qq.com>
2023-01-03 21:24:30 +0800

    参数化“我的订单-已买到的宝贝-列表查询”测试用例

--- testcases/test_search_order_buy.py
+++ testcases/test_search_order_buy.py
@@ -2,21 +2,25 @@
 # @Author:Henry
 from time import sleep
 
-
+import pytest
 from page.LeftMenuPage import LeftMenuPage
 from page.LoginPage import LoginPage
 from page.OrderPage import OrderPage
 
+# 参数化
+# 打开浏览器点击全部，然后退出，然后再打开，点击待付款，然后退出...
+
+tab_list = ["全部", "待付款", "待发货", "运输中", "待确认", "待评价"]
 
 class TestOrderBuy:
-
-    def test_order_buy(self,driver):
+    @pytest.mark.parametrize("tab",tab_list)
+    def test_order_buy(self,driver,tab):
         LoginPage().login(driver, "jay")
         LeftMenuPage().click_level_one_menu(driver, "我的订单")
         sleep(1)
         LeftMenuPage().click_level_two_menu(driver, "已买到的宝贝")
         sleep(2)
-        tab_list = ["全部", "待付款", "待发货", "运输中", "待确认", "待评价"]
-        for tab in tab_list:
-            OrderPage().click_order_tab(driver, tab)
-            sleep(2)
+        # tab_list = ["全部", "待付款", "待发货", "运输中", "待确认", "待评价"]
+        # for tab in tab_list:
+        OrderPage().click_order_tab(driver, tab)
+        sleep(2)
